6937030abf0e88abded953ced87309dff3f203be
robertwwalker <rwalker@willamette.edu>
2016-10-13 20:43:16 -0700

    Add files via upload

---
+++ fImport-Equities.R
@@ -0,0 +1,24 @@
+pkgTest <- function(x)
+  {
+    if (!require(x,character.only = TRUE))
+    {
+      install.packages(x,dep=TRUE)
+        if(!require(x,character.only = TRUE)) stop("Package not found")
+    }
+  }
+pkgTest("fImport")
+DL.Cleaner <- function(ticker) {
+  Eq.Data <- yahooSeries(ticker, from="2015-08-01", to="2015-10-30", frequency="daily")
+  Eq.Data.S <- Eq.Data[,6]
+  Equities.Data.1 <- (Eq.Data.S-lag(Eq.Data.S,1))/lag(Eq.Data.S,1)
+  Daily.Ret.Adj.Close <- na.omit(as.data.frame(Equities.Data.1))
+  names(Daily.Ret.Adj.Close) <- ticker
+  Daily.Ret.Adj.Close
+}
+GSPC <- data.frame(DL.Cleaner("^GSPC"))
+WF <- data.frame(DL.Cleaner("WF"))
+YHOO <- data.frame(DL.Cleaner("YHOO"))
+Stocks <- data.frame(GSPC,WF,YHOO)
+# To add a new one, give it an object name and a ticker, let's try Amazon
+AMZN <- data.frame(DL.Cleaner("AMZN"))
+Stocks <- data.frame(AMZN,GSPC,WF,YHOO)
